69ebdc4dacf79fc27aea9794fc4dbb6646333daf
vadim rayinchick <vadim.rayinchick@gmail.com>
2018-08-27 15:16:20 -0700

    first custom edit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
+    <h1>Hello World!!!</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>this is Vadzim's JAMstack Hackathon first application</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
